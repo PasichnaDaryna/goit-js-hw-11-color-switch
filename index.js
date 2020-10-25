ef607c68e7dedf5ad5023568d17e89e9fe52296a
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2020-10-25 14:38:26 +0200

    Colorswitch

---
+++ index.js
@@ -0,0 +1,51 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"]'),
+    stopBtn: document.querySelector('button[data-action="stop"]'),
+    bodyElem: document.querySelector('body')
+}
+
+let intervalId = '';
+refs.stopBtn.disabled = true;
+
+
+refs.startBtn.addEventListener('click', onStartButtonClick);
+refs.stopBtn.addEventListener("click", onStopButtonClick);
+
+function onStartButtonClick() {
+
+    refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+    intervalId = setInterval(bodyColorSwatch, 1000)
+}
+
+
+function onStopButtonClick() {
+   refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(intervalId);
+    
+}
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+
+function bodyColorSwatch() {
+    refs.bodyElem.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+
+
